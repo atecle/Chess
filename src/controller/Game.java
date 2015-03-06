704d00b1ead0de71684bfee57cd3938828aa6346
Adam <adam.tecle@gmail.com>
2015-03-06 16:28:03 -0500

    Initial commit

---
+++ src/controller/Game.java
@@ -0,0 +1,13 @@
+package controller;
+
+import model.Player;
+import model.Square;
+
+public class Game {
+
+	Square[][] board;
+	Player player1;
+	Player player2;
+	
+	
+}
---
+++ src/view/Display.java
@@ -0,0 +1,6 @@
+package view;
+
+//initial thoughts -- after performing game logic associated with a move in Game, this class would display the results of this logic 
+public class Display {
+
+}
